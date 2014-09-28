88d681f944994405ada42954aab59ed99de66528
Marcus Fedarko <marcus.fedarko@gmail.com>
2014-09-28 10:12:29 -0400

    Added toast notifications and explicit min/max player limits to the player selection system

--- src/com/mfedarko/smspidemic/MainActivity.java
+++ src/com/mfedarko/smspidemic/MainActivity.java
@@ -7,10 +7,10 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -40,7 +40,12 @@ public class MainActivity extends Activity {
 	public void start_game(View v) {
 		
 		if(players.size()<10){
-			System.out.println("You must have 10 or more people to begin.");
+			Toast t = Toast.makeText(
+					getApplicationContext(),
+					"Between 10 and 16 players must be present to start a game.", 
+					Toast.LENGTH_LONG
+				);
+			t.show();
 			return;
 		}
 		String text="Roles have been assigned. They are Visible for demo purposes:\n";
@@ -56,6 +61,16 @@ public class MainActivity extends Activity {
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode == PICK_CONTACT_REQUEST) {
 			if (resultCode == RESULT_OK) {
+				if (players.size() >= 16) {
+					Toast t = Toast.makeText(
+						getApplicationContext(),
+						"Can't add players: max limit (16) of players reached.", 
+						Toast.LENGTH_LONG
+					);
+					t.show();
+					return;
+				}
+				
 				// Contact successfully picked!
 				Uri contactUri = data.getData();
 				// 1. Select Phone Number
@@ -72,24 +87,29 @@ public class MainActivity extends Activity {
 				int col = cursor.getColumnIndex(Phone.NUMBER);
 				
 				String phone_number = cursor.getString(col);
-				
-				for(int i=0;i<players.size();i++){
-					if(players.get(i).getPhone().equals(phone_number)){
-						System.out.println("YOU CANT DO THAT STUPID");
-						return;
-					}
-				}
 				/**------------**/
 				cursor = getContentResolver().query(
 						contactUri, p2, null, null, null
 					);
 					
-					cursor.moveToFirst();
-	
-					int col2 = cursor.getColumnIndex(Phone.DISPLAY_NAME);
-					
-					String name = cursor.getString(col2);
+				cursor.moveToFirst();
+
+				int col2 = cursor.getColumnIndex(Phone.DISPLAY_NAME);
 				
+				String name = cursor.getString(col2);
+
+				for(int i=0; i<players.size(); i++){
+					if((players.get(i).getPhone().equals(phone_number)) ||
+						(players.get(i).getName().equals(name))) {
+						Toast t = Toast.makeText(
+							getApplicationContext(),
+							"ERROR: Duplicate detected.", 
+							Toast.LENGTH_LONG
+						);
+						t.show();
+						return;
+					}
+				}
 				// 2. Select Name
 //				int nameCol = cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
 //				String name = cursor.getString(nameCol);
@@ -98,6 +118,14 @@ public class MainActivity extends Activity {
 				players.add(p);
 				playerNames+=p.getName()+"\n";
 				results.setText(playerNames);
+				Toast t = Toast.makeText(
+						getApplicationContext(),
+						String.format("Player %d of 16 added.",
+							players.size()
+						), 
+						Toast.LENGTH_LONG
+					);
+					t.show();
 			//	System.out.println(p.getName()+":DELETE PHONE LATER:::"+p.getPhone());
 			}
 		}
