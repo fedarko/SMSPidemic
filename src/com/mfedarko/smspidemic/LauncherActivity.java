1f0e861708e3dedfbeebc0438542b58b54567986
Marcus Fedarko <marcus.fedarko@gmail.com>
2014-09-27 23:52:30 -0400

    Added dedicated launcher screen activity and game status activity

---
+++ src/com/mfedarko/smspidemic/LauncherActivity.java
@@ -0,0 +1,29 @@
+package com.mfedarko.smspidemic;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * This is the activity that's run on application startup.
+ */
+public class LauncherActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_launcher);
+	}
+	
+	public void goToStartGame(View v) {
+		Intent i = new Intent(getApplicationContext(), MainActivity.class);
+		startActivity(i);
+	}
+	
+	public void goToGameStatus(View v) {
+		Intent i = new Intent(getApplicationContext(), GameStatusActivity.class);
+		startActivity(i);
+		
+	}
+}
