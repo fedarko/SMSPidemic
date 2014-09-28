66ce5cb3ff70a0537a1c49d7a937340e09d07b5c
Marcus Fedarko <marcus.fedarko@gmail.com>
2014-09-28 08:31:32 -0400

    Added basis for "add user as player" dialog

---
+++ src/com/mfedarko/smspidemic/AddUserAsPlayerActivity.java
@@ -0,0 +1,13 @@
+package com.mfedarko.smspidemic;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddUserAsPlayerActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add_user_as_player);
+	}
+}
